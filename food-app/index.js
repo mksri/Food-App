d2a63e55d9ec1f3750b8bb2360b6ebfd80fd6ba7
mksri <monikait3038@gmail.com>
2026-01-31 12:55:45 +0530

    First food app

---
+++ food-app/index.js
@@ -0,0 +1,9 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+
+const header= React.createElement("h1",{},"First REact food app");
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(header);
